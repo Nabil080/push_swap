0e397e07bfa5d7d2986a3ca298b7dc5e3b76547f
Nabil080 <bellilanabil8@gmail.com>
2024-06-13 21:32:46 +0200

    solve 3 + push 2 b done, manque a push correctement le reste dans b

--- srcs/solver.c
+++ srcs/solver.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 20:15:26 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/13 21:03:10 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/13 21:31:39 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,19 @@ static void	solve_size_3(t_list **a, t_list **b)
 
 static void	find_next_operation(t_list **a, t_list **b)
 {
+	if (ft_lstsize(*a) == 2)
+		sa(a, b);
 	if (ft_lstsize(*a) == 3)
 		solve_size_3(a, b);
+	if (ft_lstsize(*a) > 3 && ft_lstsize(*b) < 2)
+		pb(a, b);
+	//TODO : trouver quel noeud de a push dans b
+	// * -> b est trie jusqu'a ce que a tombe a 3elements et se trie
+	// if (ft_lstsize(*b) > 2)
+	// 	find_best_pb(a, b);
+	//TODO : si a est sort (3elem) on push de b dans a au bon endroit
+	// * -> a est trie, done gg wp ez
+
 }
 
 void	push_swap_sort(t_list **a, t_list **b)

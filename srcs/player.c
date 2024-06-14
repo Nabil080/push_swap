7547de239a061a314bfa21530c072e763a5dc4de
Nabil080 <nbellila@student.42.fr>
2024-06-14 16:06:25 +0200

    make play

---
+++ srcs/player.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 17:19:49 by nbellila          #+#    #+#             */
+/*   Updated: 2024/06/14 16:00:07 by nbellila         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char *argv[])
+{
+	t_list	*a;
+	t_list	*b;
+
+	args_check(argc, argv);
+	a = parse_as_lst(argc, argv);
+	if (!a)
+		exit_malloc();
+	b = NULL;
+	get_user_input(&a, &b);
+	exit_success(a, b);
+}
--- srcs/solver.c
+++ srcs/solver.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 20:15:26 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/14 15:32:43 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/14 15:53:27 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,38 +53,48 @@ static void find_best_pa(t_list **a, t_list **b)
 {
 	int	top_a;
 	int	top_b;
-// trouver le nbr de a avec la plus petite difference de top_b
-// rotate pour que ce nbr soit en haut 
+	
 	top_a = *(int *)ft_lstlast(*a)->content;
 	top_b = *(int *)ft_lstlast(*b)->content;
+	//todo trouver le nbr de a avec la plus petite difference de top_b
+	//todo rotate pour que ce nbr soit en haut 
+	ft_printf("\n--------------------DEBUG--------------------\n\n");
 	ft_printf("TOP A : %d, TOP B : %d\n", top_a, top_b);
+	ft_printf("\n--------------------DEBUG--------------------\n\n");
 }
 
 static void	find_next_operation(t_list **a, t_list **b)
 {
-	// si a = 2, trie done
+	//? si a = 2, trie done
 	if (ft_lstsize(*a) == 2)
 		sa(a, b);
-	// si a = 3, trie done
+	//? si a = 3, trie done
 	else if (ft_lstsize(*a) == 3)
 		solve_size_3(a, b);
-	// si a > 3, push dans b au bon endroit
-	else if (!is_sorted(a, NULL))
+	//todo si a > 3, push dans b au bon endroit
+	else
 		find_best_pb(a, b);
 }
 
 void	push_swap_sort(t_list **a, t_list **b)
 {
 	show_stacks(a, b);
-	//? tant que a n'est pas trie, on cherche la meilleure operation
+	//todo tant que a n'est pas trie, on cherche la meilleure operation
 	while (!is_sorted(a, NULL))
 		find_next_operation(a, b);
 	//? trouve le premier push dans a
 	find_best_pa(a, b);
 	//? tant que b n'est pas vide, on push dans a
-	//while (ft_lstsize(*b) > 0)
-	//	pa(a, b);
-	//? une fois que tout est au bon endroit dans a, on termine le tri (ra/rra)
+	while (ft_lstsize(*b) > 0)
+		pa(a, b);
+	//todo une fois que tout est au bon endroit dans a, on termine le tri (ra/rra)
 	// while (!is_sorted(a, NULL))
-	// 	ra(a, b);
+	// {
+	// 	// si le min est plus proche de la fin, ra
+	// 	if (ft_lstindex(stack_min(a)) > ft_lstsize(*a) / 2)
+	// 		ra(a, b);
+	// 	// si le min est plus proche du debut, rra
+	// 	else
+	// 		rra(a, b);
+	// }
 }
